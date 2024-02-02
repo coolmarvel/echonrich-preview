e55475e8d75ae3fbbc7cd88bbc0f68602bba6b58
coolmarvel <marvel97@naver.com>
2024-02-02 16:35:53 +0900

    @nestjs/swagger lib install to api docs & test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('EchonRich Preview Test').setDescription('').setVersion('1.0.0').build();
+  const customOptions: SwaggerCustomOptions = { swaggerOptions: { persistAuthorization: true } };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, customOptions);
+
   await app.listen(3000);
 }
 bootstrap();
