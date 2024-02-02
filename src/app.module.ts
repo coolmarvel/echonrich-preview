b09b7dfaae46e5cce81197eea30ab859a9136f70
coolmarvel <marvel97@naver.com>
2024-02-02 15:05:56 +0900

    mysql, typeorm, config lib install & apply

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { EmployeesModule } from './employees/employees.module';
+import mysqlConfig from './config/mysql.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [mysqlConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        let obj: TypeOrmModuleOptions = {
+          type: 'mysql',
+          host: configService.get('mysql.host'),
+          port: configService.get('mysql.port'),
+          database: configService.get('mysql.database'),
+          username: configService.get('mysql.username'),
+          password: configService.get('mysql.password'),
+          autoLoadEntities: true,
+          synchronize: false,
+        };
+
+        return obj;
+      },
+    }),
+    EmployeesModule,
+  ],
   controllers: [],
   providers: [],
 })
