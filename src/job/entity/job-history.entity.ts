529d024828cb21c1e8d2e708811d14a77a3ac59b
coolmarvel <marvel97@naver.com>
2024-02-02 20:28:21 +0900

    job service(특정 사원의 이력 정보 조회) API initial commit

--- src/job/entity/job-history.entity.ts
+++ src/job/entity/job-history.entity.ts
@@ -2,12 +2,12 @@ import { Employee } from 'src/employee/entity/employee.entity';
 import { Job } from './job.entity';
 import { Department } from 'src/department/entity/department.entity';
 
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('job_history')
 export class JobHistory {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn()
+  employee_id: number;
 
   @ManyToOne(() => Employee, (employee) => employee.jobHistories)
   @JoinColumn({ name: 'employee_id' })
