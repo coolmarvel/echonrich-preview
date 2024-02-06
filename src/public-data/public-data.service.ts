5a55c33e436cbc382a716e7229e5ed8d202c1f95
coolmarvel <marvel97@naver.com>
2024-02-06 15:13:38 +0900

    boxoffice, movie search params divorce

--- src/public-data/public-data.service.ts
+++ src/public-data/public-data.service.ts
@@ -16,7 +16,7 @@ export class PublicDataService {
   async getDailyBoxOfficeList({ targetDt, itemPerPage, multiMovieYn, repNationCd, wideAreaCd }: DailyBoxOfficeReqDto) {
     const params = { key: this.apiKey, targetDt, itemPerPage, multiMovieYn, repNationCd, wideAreaCd };
 
-    const result = this.httpService.get(this.url + 'searchDailyBoxOfficeList.json', { params });
+    const result = this.httpService.get(this.url + 'boxoffice/searchDailyBoxOfficeList.json', { params });
 
     return result.toPromise().then((response) => response.data);
   }
@@ -24,7 +24,7 @@ export class PublicDataService {
   async getWeeklyBoxOfficeList({ targetDt, weekGb, itemPerPage, multiMovieYn, repNationCd, wideAreaCd }: WeeklyBoxOfficeReqDto) {
     const params = { key: this.apiKey, targetDt, weekGb, itemPerPage, multiMovieYn, repNationCd, wideAreaCd };
 
-    const result = this.httpService.get(this.url + 'searchWeeklyBoxOfficeList.json', { params });
+    const result = this.httpService.get(this.url + 'boxoffice/searchWeeklyBoxOfficeList.json', { params });
 
     return result.toPromise().then((response) => response.data);
   }
@@ -32,7 +32,7 @@ export class PublicDataService {
   async getMovieList({ curPage, itemPerPage, movieNm, directorNm, openStartDt, openEndDt, prdtStartYear, prdtEndYear, repNationCd, movieTypeCd }: MovieListReqDto) {
     const params = { key: this.apiKey, curPage, itemPerPage, movieNm, directorNm, openStartDt, openEndDt, prdtStartYear, prdtEndYear, repNationCd, movieTypeCd };
 
-    const result = this.httpService.get(this.url + 'searchMovieList.json', { params });
+    const result = this.httpService.get(this.url + 'movie/searchMovieList.json', { params });
 
     return result.toPromise().then((response) => response.data);
   }
